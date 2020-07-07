d4576571ec0479af7262bb273e39bcec165a176e
evilbunnyrabbits <evilbunnyrabbits@gmail.com>
2020-07-07 12:23:49 -0400

    first commit

---
+++ db/migrate/20200707162026_first_migration.rb
@@ -0,0 +1,4 @@
+class FirstMigration < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
---
+++ db/migrate/20200707162207_create_restaurant_table.rb
@@ -0,0 +1,7 @@
+class CreateRestaurantTable < ActiveRecord::Migration[5.2]
+  def change
+    create_table :restaurants do |t|
+      t.string :name
+    end
+  end
+end
